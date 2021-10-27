9acc271ffc2948c16bb90308ad8f515cb8585086
agudys <adam.gudys@gmail.com>
2021-10-27 12:57:13 +0200

    Matcher finished (not tested yet).

--- utils/matcher.cpp
+++ utils/matcher.cpp
@@ -22,9 +22,36 @@ program. If not, see https://www.gnu.org/licenses/.
 #include <fstream>
 #include <sstream>
 #include <unordered_set>
-#include <unordered_map>
+#include <map>
+#include <chrono>
+#include <iostream>
+
 
 using namespace std;
+
+union GenomeCoords {
+	struct {
+		uint32_t pos;
+		uint16_t chr;
+		uint16_t is_rev;
+	};
+
+	uint64_t raw;
+};
+
+struct Match {
+	uint32_t vir_start;
+	uint32_t vir_last;
+	GenomeCoords host_start;
+	GenomeCoords host_last;
+
+	Match(uint32_t vir_start, uint32_t vir_last, GenomeCoords host_start, GenomeCoords host_last) :
+		vir_start(vir_start),
+		vir_last(vir_last),
+		host_start(host_start),
+		host_last(host_last) 
+	{}
+};
 
 
 bool findSwitch(std::vector<std::string>& params, const std::string& name) {
@@ -52,21 +79,44 @@ bool findOption(std::vector<std::string>& params, const std::string& name, T& v)
 
 int main(int argc, char** argv) {
 
+	cout << "PHIST-Matcher utility 1.0.0" << endl
+		<< "A.Zielezinski, S. Deorowicz, A. Gudys (c) 2021" << endl << endl;
+	
 	std::vector<std::string> params(argc - 1);
 	std::transform(argv + 1, argv + argc, params.begin(), [](char* s)->string { return s; });
 
 	int k;
 	if (!findOption(params, "-k", k)) {
-		return -1;
+		k = 25;
 	}
+
+	if (params.size() != 3) {
+		cout << "USAGE:" << endl
+			<< "matcher [-k <length>] <phage> <host> <matches>" << endl << endl
+			<< "Parameters:" << endl
+			<< "\tlength - minimum match length (25 by default)" << endl
+			<< "\tphage - phage FASTA file (gzipped or not)" << endl
+			<< "\thost - host FASTA file (gzipped or not)" << endl
+			<< "\tmatches - all exact matches of length k or longer" << endl;
+		return 0;
+	}
+
+	auto start = std::chrono::high_resolution_clock::now();
 
 	const std::string& virPath = params[0];
 	const std::string& hostPath = params[1];
 
+	cout << "Finding exact matches..." << endl
+		<< "minimum length: " << k << endl
+		<< "phage FASTA:    " << virPath << endl
+		<< "host FASTA:     " << hostPath << endl  << endl;
+
 	FastaFile virFasta;
 	FastaFile hostFasta;
-	virFasta.open(virPath);
-	hostFasta.open(hostPath);
+	if (!virFasta.open(virPath) || !hostFasta.open(hostPath)) {
+		cout << "Unable to open input files" << endl;
+		return -1;
+	}
 
 	std::vector<std::vector<kmer_t>> virKmerCollections(virFasta.numSubsequences());
 	std::unordered_set<kmer_t> uniqueKmers; // this set will be used for filtering host kmers
@@ -92,12 +142,12 @@ int main(int argc, char** argv) {
 	}
 
 	// iterate over host subsequences
-	std::unordered_multimap<kmer_t, uint64_t> hostKmersFov;
-	std::unordered_multimap<kmer_t, uint64_t> hostKmersRev;
+	std::multimap<kmer_t, GenomeCoords> hostKmers;
+
 
 	SetBasedFilter filter(uniqueKmers);
 
-	for (uint64_t chr_id = 0; chr_id < hostFasta.numSubsequences(); ++chr_id) {
+	for (uint16_t chr_id = 0; chr_id < hostFasta.numSubsequences(); ++chr_id) {
 		std::vector<kmer_t> kmers(hostFasta.getLengths()[chr_id] - k + 1);
 		std::vector<uint32_t> positions(hostFasta.getLengths()[chr_id] - k + 1);
 
@@ -111,8 +161,8 @@ int main(int argc, char** argv) {
 			positions.data());
 
 		for (int i = 0; i < count; ++i) {
-			uint64_t global_pos = chr_id << 32 | positions[i];
-			hostKmersFov.insert(std::make_pair(kmers[i], global_pos));
+			GenomeCoords coords = { positions[i], chr_id, 0 };
+			hostKmers.insert(std::make_pair(kmers[i], coords));
 		}
 
 		// reverse direction
@@ -125,8 +175,8 @@ int main(int argc, char** argv) {
 			positions.data());
 
 		for (int i = 0; i < count; ++i) {
-			uint64_t global_pos = (1LL << 63) | (chr_id << 32) | positions[i];
-			hostKmersFov.insert(std::make_pair(kmers[i], global_pos));
+			GenomeCoords coords = { positions[i], chr_id, 1 };
+			hostKmers.insert(std::make_pair(kmers[i], coords));
 		}
 	}
 
@@ -134,53 +184,87 @@ int main(int argc, char** argv) {
 	ofstream outfile(params[2]);
 	outfile << "phage,host" << endl;
 
+	std::vector<Match> matches;
+
+	// iterate over virus chromosomes
 	for (int vir_cid = 0; vir_cid < virKmerCollections.size(); ++vir_cid) {
 		const auto& col = virKmerCollections[vir_cid];
 		string vir_header = virFasta.getHeaders()[vir_cid];
 		
-		uint64_t vir_match_start = 0;
-		uint64_t vir_match_last = 0;
-		
-		for (uint64_t i = 0; i < col.size(); ++i) {
+		// iterate over virus positions
+		for (uint64_t vir_pos = 0; vir_pos < col.size(); ++vir_pos) {
 
-			kmer_t kmer = col[i];
-			auto it = hostKmersFov.find(kmer);
+			// get host 
+			kmer_t kmer = col[vir_pos];
+			auto range = hostKmers.equal_range(kmer);
 			
-			if (it != hostKmersFov.end()) {
-				uint64_t vir_pos = i + 1; // 1-based indexing
+			if (range.first != range.second) {
+				
+				// iterate over hit range of host positions
+				for (auto it = range.first; it != range.second; ++it) {
+					GenomeCoords host_hit = it->second;
+					
+					bool consumed = false;
 
-				if (vir_match_start == 0) {
-					// start match region
-					vir_match_start = vir_match_last = vir_pos;
+					// iterate over current matches and check if hit extends any of it
+					for (auto& match : matches) {
+						if (host_hit.raw == match.host_last.raw - 1 && host_hit.is_rev) {
+							// continue reverse match
+							--match.host_last.pos;
+							++match.vir_last;
+							consumed = true;
+						}
+						else if (host_hit.raw == match.host_last.raw + 1 && !host_hit.is_rev) {
+							// continue forward match
+							++match.host_last.pos;
+							++match.vir_last;
+							consumed == true;
+						}
+					}
+
+					// if hit does not extend any existing match - create a new one
+					if (!consumed) {
+						matches.emplace_back(vir_pos, vir_pos, host_hit, host_hit);
+					}
+
+				}
+			}
+
+			// save and remove unextended matches
+			for (auto it = matches.begin(); it != matches.end(); )
+			{
+				if (it->vir_last != vir_pos) {
+					
+					std::pair<uint32_t, uint32_t> vir_range{ it->vir_start + 1, it->vir_last + k }; // 1-based indexing
+					std::pair<uint32_t, uint32_t> host_range;
+					
+					
+					if (it->host_last.is_rev) {
+						host_range.second = it->host_last.pos + 1; // 1-based indexing
+						host_range.first = it->host_start.pos + k;
+					}
+					else {
+						host_range.first = it->host_start.pos + 1; // 1-based indexing
+						host_range.second = it->host_last.pos + k;
+					}
+
+					outfile
+						<< vir_header << ':' << vir_range.first << "-" << vir_range.second << ","  
+						<< hostFasta.getHeaders()[it->host_last.chr] << ":" << host_range.first << "-" << host_range.second << endl;
+
+					it = matches.erase(it);
 				}
 				else {
-					// continue match region
-					vir_match_last = vir_pos;
+					++it;
 				}
-
-				uint64_t host_pos = it->second;
-
-				bool is_rev = host_pos >> 63;
-				host_pos = (host_pos << 1) >> 1; // clear MSB
-				uint64_t host_chr_id = host_pos >> 32;
-				host_pos = (host_pos << 32) >> 32; // clear upper half
-	
-			}
-			else {
-				// check if region has just finished
-				if (vir_match_start) {
-					outfile
-						<< vir_header << ':' << vir_match_start << "-" << vir_match_last + k - 1 << "," << endl;
-						//<< hostFasta.getHeaders()[host_chr_id] << ":" << host_pos << "-" << host_pos + k - 1 << endl;
-					vir_match_start = vir_match_last = 0;
-				}
-			}
-
-			
+			}	
 		}
 	} 
 
 	outfile.close();
+
+	auto time = std::chrono::duration<double>(std::chrono::high_resolution_clock::now() - start);
+	cout << "Finished in " << time.count() << " seconds" << endl;
 
 	return 0;
 }
